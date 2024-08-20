c73a39645ee7b972674f3376097e829f096738ab
Eric Jacobsen <eric@whiskyvangoghgo.com>
2024-08-20 18:41:41 -0400

    more build

--- package.config.js
+++ package.config.js
@@ -1,9 +1,8 @@
-import {defineConfig} from '@sanity/pkg-utils'
+import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
   legacyExports: true,
   dist: 'dist',
-  tsconfig: 'tsconfig.dist.json',
 
   // Remove this block to enable strict export validation
   extract: {
